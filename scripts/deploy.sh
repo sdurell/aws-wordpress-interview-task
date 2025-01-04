2d97439f0b44715af6adb9ca5ae6502f1abdbf74
sdurell <spencerdurell@gmail.com>
2025-01-04 09:48:26 -0500

    Fixing deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Deploying Wordpress"
-unzip -o /tmp/wordpress.zip -d /var/www/html/wordpress
+unzip -o /var/temp/wordpress.zip -d /var/www/html/wordpress
 chown -R apache:apache /var/www/html/wordpress
