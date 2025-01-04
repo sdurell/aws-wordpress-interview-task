a5ed2aac7060e132f84d7618ca01358825f5b7a4
sdurell <spencerdurell@gmail.com>
2025-01-04 09:37:06 -0500

    fixing cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Cleaning up old Wordpress files"
-find "/var/www/html/wordpress/wp-content" -mindepth 1 ! -name "uploads" -exec rm -rf {} \;
+find "/var/www/html/wordpress/wp-content" -mindepth 1 ! -name "uploads" -exec rm -f {} \;
